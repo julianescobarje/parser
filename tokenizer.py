f0fd6f1b569f9850306c32bd6171f8bbdee55a44
Julian Escobar <escobarjulian347@gmail.com>
2023-02-09 16:28:59 -0500

    Se toman en cuenta los espacios

--- tokenizer.py
+++ tokenizer.py
@@ -1,9 +1,12 @@
 class Token:
-    def __init__(self, type_, value):
+    def __init__(self, type_: str, value):
         self.type = type_
         self.value = value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.value:
             return f'{self.type}:{self.value}'
         return f'{self.type}'
+
+    def getType(self) -> str:
+        return self.type
